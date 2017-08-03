40909e451d9ea00ad0636f32c178af9cd0c0dafc
kailinc <kailinc@bu.edu>
2017-08-03 16:32:26 -0400

    Replaced all instances of express-api-template

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const mongooseBaseName = 'express-api-template'
+const mongooseBaseName = 'express-multer-upload-api'
 
 const environment = process.env.NODE_ENV || 'development'
 const database = {
