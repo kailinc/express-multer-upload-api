a72644b073470bb0e2a830d663ba822908cf6896
kailinc <kailinc@bu.edu>
2017-08-04 11:18:16 -0400

    Closed Database for running script in command line

--- bin/aws-3-upload.js
+++ bin/aws-3-upload.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const AWSUpload = require('../lib/s3-uploads')
+const mongoose = require('app/middleware/mongoose')
 
 const file = process.argv[2]
 const name = process.argv[3]
@@ -8,3 +9,4 @@ const name = process.argv[3]
 AWSUpload({ file, name })
   .then(console.log)
   .catch(console.error)
+  .then(() => mongoose.connection.close())
